48a55e378e1aa9f663092edc486f243c224d75ae
Jiahao Li <jiahaoli0465@gmail.com>
2024-01-24 23:00:38 -0500

    finished machine

--- markov.js
+++ markov.js
@@ -1,6 +1,3 @@
-/** Textual markov chain generator */
-
-
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
@@ -14,16 +11,50 @@ class MarkovMachine {
   /** set markov chains:
    *
    *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+   *  {"the cat": ["in"], "cat in": ["the"], "in the": ["hat"], "the hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length - 1; i += 1) {
+      let bigram = this.words[i] + " " + this.words[i + 1];
+      let nextWord = this.words[i + 2] || null;
+
+      if (chains.has(bigram)) chains.get(bigram).push(nextWord);
+      else chains.set(bigram, [nextWord]);
+    }
+
+    this.chains = chains;
+  }
+
+
+  /** Pick random choice from array */
+
+  choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    // pick a random key to begin
+    let keys = Array.from(this.chains.keys());
+    let key = this.choice(keys);
+    let out = [];
+
+    // produce markov chain until reaching termination word
+    while (out.length <= numWords && key !== null) {
+      let [w1, w2] = key.split(" ");
+      out.push(w1);
+      key = w2 + " " + this.choice(this.chains.get(key));
+    }
+
+    return out.join(" ");
   }
 }
+
+
+module.exports = {
+  MarkovMachine,
+};
